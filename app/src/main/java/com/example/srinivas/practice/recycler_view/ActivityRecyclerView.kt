5b139ef003816bf50a4804609c93d730f4b26382
srinivas2794 <hereiamsrinivas@gmail.com>
2017-07-03 12:48:44 +0530

    refactoring

--- app/src/main/java/com/example/srinivas/practice/recycler_view/ActivityRecyclerView.kt
+++ app/src/main/java/com/example/srinivas/practice/recycler_view/ActivityRecyclerView.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import com.example.srinivas.practice.R
 import kotlinx.android.synthetic.main.activity_recycler_view.*
 
 
@@ -17,7 +22,32 @@ class ActivityRecyclerView : AppCompatActivity() {
         mLinearLayoutManager = LinearLayoutManager(this)
         recycler_view.layoutManager = mLinearLayoutManager
     }
+}
+
+private class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        val item_recycler_view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_recycler_view, parent, false)
+        return RecyclerViewHolder(item_recycler_view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 
+    class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val textView: TextView = view.findViewById(R.id.home_list) as TextView
+
+        init {
+            view.setOnClickListener { Toast.makeText(view.context, "hello", Toast.LENGTH_LONG) }
+        }
+
+    }
 
 }
